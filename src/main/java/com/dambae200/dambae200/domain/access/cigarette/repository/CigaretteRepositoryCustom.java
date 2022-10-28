b7dc4276cf40b227a0d03d583b39e47a1196bcf3
chowi29 <104131558+chowi29@users.noreply.github.com>
2022-10-28 09:44:20 +0900

    Delete src/main/java/com/dambae200/dambae200/domain/access/cigarette directory

--- src/main/java/com/dambae200/dambae200/domain/access/cigarette/repository/CigaretteRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package dambae200.dambae200.domain.cigarette.repository;
-
-import dambae200.dambae200.domain.cigarette.domain.Cigarette;
-
-public interface CigaretteRepositoryCustom {
-    Cigarette findOneByIdCustom(Long id);
-}
