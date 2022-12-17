328084fd577b69506ab3f551baf6ffc0668c092a
leehyeonmin34 <leehyeonmin34@gmail.com>
2022-12-18 01:32:38 +0900

    프론트 포트 변경

--- src/main/java/com/dambae200/dambae200/global/filter/CorsFilter.java
+++ src/main/java/com/dambae200/dambae200/global/filter/CorsFilter.java
@@ -21,7 +21,7 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:80");
+        response.setHeader("Access-Control-Allow-Origin", "http://118.67.135.98:80");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
