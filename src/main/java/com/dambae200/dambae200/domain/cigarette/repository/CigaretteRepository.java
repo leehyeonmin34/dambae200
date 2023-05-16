287a5267324aee2f9ec5dd8058da87290e2b7653
leehyeonmin34 <leehyeonmin34@gmail.com>
2023-05-17 02:07:36 +0900

    불필요한 CigaretteRepository 관련 클래스 및 메서드 삭제

--- src/main/java/com/dambae200/dambae200/domain/cigarette/repository/CigaretteRepository.java
+++ src/main/java/com/dambae200/dambae200/domain/cigarette/repository/CigaretteRepository.java
@@ -1,21 +1,8 @@
 package com.dambae200.dambae200.domain.cigarette.repository;
 
 import com.dambae200.dambae200.domain.cigarette.domain.Cigarette;
-import com.dambae200.dambae200.domain.cigarette.repository.CigaretteRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-public interface CigaretteRepository extends JpaRepository<Cigarette, Long>, CigaretteRepositoryCustom {
-    List<Cigarette> findAllByOfficialNameLike(String s);
-
+public interface CigaretteRepository extends JpaRepository<Cigarette, Long> {
     boolean existsByOfficialName(String name);
-
-    @Query("select c from Cigarette c where c.officialName = : officialName")
-    Cigarette findOneByOfficialName(@Param("officialName") String officialName);
-
-    @Query("select c from Cigarette c where c.simpleName = : simpleName")
-    Cigarette findOneBySimpleName(@Param("simpleName") String simpleName);
 }
--- src/main/java/com/dambae200/dambae200/domain/cigarette/repository/CigaretteRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.dambae200.dambae200.domain.cigarette.repository;
-
-import com.dambae200.dambae200.domain.cigarette.domain.Cigarette;
-
-public interface CigaretteRepositoryCustom {
-    Cigarette findOneByIdCustom(Long id);
-}
