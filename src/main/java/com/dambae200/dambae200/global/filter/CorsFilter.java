ed48313700a8d71878772f974171092b40e95f06
leehyeonmin34 <leehyeonmin34@gmail.com>
2023-06-06 16:59:13 +0900

    cors 정책 수정

--- src/main/java/com/dambae200/dambae200/global/filter/CorsFilter.java
+++ src/main/java/com/dambae200/dambae200/global/filter/CorsFilter.java
@@ -22,7 +22,8 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 //        response.setHeader("Access-Control-Allow-Origin", "http://118.67.135.98:80");
-        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+//        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
