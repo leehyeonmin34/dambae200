376ade489eaea93164d146c70ad9fa24e4937850
leehyeonmin34 <leehyeonmin34@gmail.com>
2023-06-07 18:07:16 +0900

    cors 수정

--- src/main/java/com/dambae200/dambae200/global/common/config/WebConfig.java
+++ src/main/java/com/dambae200/dambae200/global/common/config/WebConfig.java
@@ -1,11 +1,12 @@
 package com.dambae200.dambae200.global.common.config;
 
 import com.dambae200.dambae200.global.filter.CorsFilter;
+import com.dambae200.dambae200.global.interceptor.CorsInterceptor;
 import com.dambae200.dambae200.global.interceptor.StoreAccessCheckInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,24 +14,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH,CUSTOM";
+//    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH,CUSTOM";
     final private StoreAccessCheckInterceptor storeAccessCheckInterceptor;
+//    final private CorsInterceptor corsInterceptor;
 
-    @Override
+//    @Override
     // CORS 정책을 허용해줄 url과 메서드를 재정의해준다
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**") // url path
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(",")) // 메서드 종류
-                .exposedHeaders(HttpHeaders.LOCATION)// 서버에 반환해줄 헤더 지정
-                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "storeId")
-        ;
-
-        // origin은 별도 명시를 안하면 DEFAULT_PERMIT_ALL로 모두 허용하게 된다.
-    }
+//    public void addCorsMappings(final CorsRegistry registry) {
+//        registry.addMapping("/**") // url path
+//                .allowedMethods(ALLOWED_METHOD_NAMES.split(",")) // 메서드 종류
+//                .exposedHeaders(HttpHeaders.LOCATION)// 서버에 반환해줄 헤더 지정
+//                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "storeId")
+//        ;
+//
+//        // origin은 별도 명시를 안하면 DEFAULT_PERMIT_ALL로 모두 허용하게 된다.
+//    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(storeAccessCheckInterceptor);
+//        registry.addInterceptor(corsInterceptor);
     }
 
 
--- src/main/java/com/dambae200/dambae200/global/interceptor/CorsInterceptor.java
+++ src/main/java/com/dambae200/dambae200/global/interceptor/CorsInterceptor.java
@@ -9,9 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 public class CorsInterceptor implements HandlerInterceptor {
 
 //    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-//        return HttpMethod.OPTIONS.matches(request.getMethod());
-//        // 요청이 OPTIONS일 경우 별도의 인증과정 없이 해당 요청을 받아들인다.
-//        // preflight엔 인증헤더가 없기 때문에, preflight를 정상처리하기 위해서 필요하다.
-//    }
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        return HttpMethod.OPTIONS.matches(request.getMethod());
+        // 요청이 OPTIONS일 경우 별도의 인증과정 없이 해당 요청을 받아들인다.
+        // preflight엔 인증헤더가 없기 때문에, preflight를 정상처리하기 위해서 필요하다.
+    }
 }
