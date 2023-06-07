59c14656ad05ed4727ac3f8b88b6150c66b95029
leehyeonmin34 <leehyeonmin34@gmail.com>
2023-06-07 19:09:01 +0900

    Cors 수정

--- src/main/java/com/dambae200/dambae200/global/filter/CorsFilter.java
+++ src/main/java/com/dambae200/dambae200/global/filter/CorsFilter.java
@@ -8,10 +8,16 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+
+    private final static String[] allowedOrigins = {"http://192.100.0.223:5500", "http://49.50.164.244:9090", "http://49.50.164.244:9999"};
+    // 로컬 VS Code
+    // ncp-main nginx
+    // ncp-main 프론트엔드 포트
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -22,14 +28,13 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-//        response.setHeader("Access-Control-Allow-Origin", "http://118.67.135.98:80");
-//        response.setHeader("Access-Control-Allow-Origin", "http://192.100.0.223:5500");
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        int idx = Arrays.asList(allowedOrigins).indexOf(request.getHeader("Origin"));
+        if(idx > -1) response.setHeader("Access-Control-Allow-Origin", allowedOrigins[idx]);
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization, storeId");
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization, StoreId");
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
