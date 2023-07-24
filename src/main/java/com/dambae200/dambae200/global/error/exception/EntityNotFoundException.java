2e3e68aa33f4dfce4418b5abe0184ee45cad5118
leehyeonmin34 <leehyeonmin34@gmail.com>
2023-07-24 17:52:44 +0900

    EntityNotfoundException 에러메시지 수정

--- src/main/java/com/dambae200/dambae200/global/error/exception/EntityNotFoundException.java
+++ src/main/java/com/dambae200/dambae200/global/error/exception/EntityNotFoundException.java
@@ -3,7 +3,7 @@ package com.dambae200.dambae200.global.error.exception;
 public class EntityNotFoundException extends BusinessException {
 
     public EntityNotFoundException(String entity, String id){
-        super(ErrorCode.ENTITY_NOT_FOUND, String.format("id %d에 해당하는 %s entity를 찾을 수 없습니다.", id, entity));
+        super(ErrorCode.ENTITY_NOT_FOUND, String.format("id %s에 해당하는 %s entity를 찾을 수 없습니다.", id, entity));
     }
 
     public EntityNotFoundException(String msg){
